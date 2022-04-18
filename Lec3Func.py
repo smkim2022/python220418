c774a4a2c5a15c35c75e98d508dcb7bf0dafa1ca
smkim2022 <smylist@naver.com>
2022-04-18 16:31:48 +0900

    함수연습1

---
+++ Lec3Func.py
@@ -0,0 +1,7 @@
+# Lec3Func.py
+
+#def times(a,b):
+#    return a*b
+#times
+#times(5,6)
+#mytimes = times
---
+++ function1.py
@@ -0,0 +1,10 @@
+# function1.py
+
+# 함수를 정의
+def setValue(newValue):
+    x = newValue
+    print("함수내부:",x)
+
+# 호출
+retValue = setValue(5)
+print(setValue)
