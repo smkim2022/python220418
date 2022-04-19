827b895543e6665938ba5021feb461994dd69c84
smkim2022 <smylist@naver.com>
2022-04-19 09:22:37 +0900

    0419-01-02

--- function1.py
+++ function1.py
@@ -9,3 +9,18 @@ def setValue(newValue):
 # 디비깅할 때 중단점
 retValue = setValue(5)
 print(retValue)
+
+# 교집합 문자를 리턴하는 함수
+def intersect(prelist, postlist):
+    #지역변수
+    result = []
+    #H(0) | A(1) | M(2)
+    for x in prelist:
+        # x라는 글자가 postlist 에 있고 그리고 x result 에 없으면
+        # in단독으로 사용하면 include
+        if x in postlist and x not in result:
+            result.append(x)
+    return result
+
+# 호출(중단점 추가)
+print(intersect("HAM","SPAM"))
